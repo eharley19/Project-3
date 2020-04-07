0ba6570da6127ac518fb140084155d1ee02bc0fc
arkevo <ikolev57@gmail.com>
2020-04-07 19:04:32 -0400

    Create front page components (#5)
    * Feat: Added Search and Saved Page, Added API and Paginate Utility, Added Components- Footer, Job, List, NavBar, and SearchForm, addedd CSS
    
    * Feat: fixed the forum, CSS, and added User and Register
    
    Co-authored-by: Ivo Kolev <arkevo@Ivos-MacBook-Pro.local>

---
+++ job-search-site/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer navbar-fixed-bottom has-background-cyan text-center heading-subtitle">
+      
+    </footer>
+  );
+};
+
+export default Footer;
