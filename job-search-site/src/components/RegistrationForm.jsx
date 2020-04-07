b04edfe7ff70fee6086719bdf7cdfbf283e5f40e
Edward Harley <edharley19@gmail.com>
2020-04-06 22:34:34 -0400

    Feat: Registration form.  Additional changes

--- job-search-site/src/components/RegistrationForm.jsx
+++ job-search-site/src/components/RegistrationForm.jsx
@@ -1,64 +1,10 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 import { Modal, Form, Button } from "react-bootstrap";
 import { Formik } from "formik";
 import * as Yup from "yup";
 
-const MODAL = styled.div`
-  background: #f7f9fa;
-  height: auto;
-  width: 90%;
-  margin: 5em auto;
-  color: snow;
-  -webkit-box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.4);
-  -moz-box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.4);
-  box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.4);
-  @media (min-width: 786px) {
-    width: 60%;
-  }
-  label {
-    color: #24b9b6;
-    font-size: 1.2em;
-    font-weight: 400;
-  }
-  .error {
-    border: 2px solid #ff6565;
-  }
-  .error-message {
-    color: #ff6565;
-    padding: 0.5em 0.2em;
-    height: 1em;
-    position: absolute;
-    font-size: 0.8em;
-  }
-  h1 {
-    color: #24b9b6;
-    padding-top: 0.5em;
-  }
-  .form-group {
-    margin-bottom: 2.5em;
-  }
-`;
 
-const FORM = styled(Form)`
-  width: 90%;
-  text-align: left;
-  padding-top: 2em;
-  padding-bottom: 2em;
-  @media (min-width: 786px) {
-    width: 50%;
-  }
-`;
-
-const BUTTON = styled(Button)`
-  background: #1863ab;
-  border: none;
-  font-size: 1.2em;
-  font-weight: 400;
-  &:hover {
-    background: #1d3461;
-  }
-`;
 
 
 const phoneRegExp = /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/;
@@ -81,13 +27,13 @@ const validationSchema = Yup.object().shape({
 
 const RegistrationForm = () => {
   return (
-    <Modal styles={MODAL}>
+    <Modal show={true} >
       <Modal.Header closeButton>
         <Modal.Title>Registration Form</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Formik
-          initialValues={{ name: "", email: "", phone: "", blog: "" }}
+          initialValues={{ name: "", email: "", phone: ""}}
           validationSchema={validationSchema}
           onSubmit={(values, { setSubmitting, resetForm }) => {
             setSubmitting(true);
@@ -107,7 +53,7 @@ const RegistrationForm = () => {
               handleSubmit,
               isSubmitting,
             }) => (
-            <FORM onSubmit={handleSubmit} className="mx-auto">
+            <Form onSubmit={handleSubmit} className="mx-auto">
               <Form.Group controlId="formName">
                 <Form.Label>Name :</Form.Label>
                 <Form.Control
@@ -155,10 +101,10 @@ const RegistrationForm = () => {
               </Form.Group>
 
               {/*Submit button that is disabled after button is clicked/form is in the process of submitting*/}
-              <BUTTON variant="primary" type="submit" disabled={isSubmitting}>
+              <Button variant="primary" type="submit" disabled={isSubmitting}>
                 Submit
-              </BUTTON>
-            </FORM>
+              </Button>
+            </Form>
           )}
         </Formik>
       </Modal.Body>
