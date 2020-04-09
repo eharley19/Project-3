9e48590302bc7ca393c07b0b59f359948182e4f5
eharley19 <48498743+eharley19@users.noreply.github.com>
2020-04-09 07:33:45 -0400

    Register form (#6)
    * Feat: Preliminary user moder - missing phone number validator
    
    * Feat: Registration form in moda (not currently working
    
    * Feat: Registration Form. Removed unnecessary field.
    
    * Feat: Registration form.  Additional changes
    
    * Fix: Fixed styling isssue with registration form modal
    
    * Feat: Registration form. Additional fields added
    
    * Chore: Removed styled-components package
    
    * Feat: Registration form - additional fields added.

--- job-search-site/src/components/NavBar/index.js
+++ job-search-site/src/components/NavBar/index.js
@@ -1,56 +1,76 @@
-import React from "react";
+import React, { Component } from 'react';
 import { Link, NavLink } from "react-router-dom";
+import RegistrationForm from "../RegistrationForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style.css";
-const NavBar = () => {
-  return (
-    <React.Fragment>
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <Link className="navbar-brand brand-titles" to="#">
-        Workers4Covid
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink className="nav-link heading-titles" to="/search">
-                SEARCH
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link heading-titles" to="/saved">
-                SAVED
-              </NavLink>
-            </li>
-          </ul>
-        </div>
 
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink className="nav-link heading-titles" to="/search">
-                Login
+class NavBar extends Component {
+  state = {
+    modalOpen: false
+ }
+
+ handleModalOpen = () => {
+    this.setState((prevState) => {
+       return{
+          modalOpen: !prevState.modalOpen
+       }
+    })
+ }
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <Link className="navbar-brand brand-titles" to="#">
+            Workers4Covid
+        </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link heading-titles" to="/search">
+                  SEARCH
               </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link heading-titles" to="/saved">
-                Register
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link heading-titles" to="/saved">
+                  SAVED
               </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </React.Fragment>
-  );
+              </li>
+            </ul>
+          </div>
+
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link heading-titles" to="/search">
+                  Login
+              </NavLink>
+              </li>
+              <li className="nav-item">
+                <a onClick={this.handleModalOpen} className="nav-link">
+                  Register
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <RegistrationForm
+          modalOpen={this.state.modalOpen}
+          handleModalOpen={this.handleModalOpen}
+        />
+      </React.Fragment>
+    )
+  }
 };
 
 export default NavBar;
