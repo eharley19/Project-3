3354ae9039d1c2f0ecadb25803fa331734e513e4
arkevo <ikolev57@gmail.com>
2020-04-09 22:18:07 -0400

    Heroku and Backend (#10)
    * initial commit
    
    * adding to heroku
    
    * heroku
    
    Co-authored-by: Ivo Kolev <arkevo@Ivos-MacBook-Pro.local>

---
+++ models/job.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const jobSchema = new Schema({
+  title: { type: String, required: true },
+  location: { type: String },
+  contract_time: { type: [String], required: true },
+  link: { type: String, required: true },
+  description: { type: String, required: true },
+  company: { type: String, required: true },
+  adzunaId: { type: String, required: true, unique: true }
+});
+
+const Job = mongoose.model("Job", jobSchema);
+
+module.exports = Job;
