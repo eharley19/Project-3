d728078163795a4c91bfddf359b82fa1986578c9
eharley19 <48498743+eharley19@users.noreply.github.com>
2020-04-16 08:49:49 -0400

    Authentication (#20)
    * Added jwt key
    
    * Event handlers added to registration form and login.
    
    * Token verification function
    
    * Verification schema
    
    * Fixed paths, installed CORS package, added auth to jobroutes
    
    * Cors implemented in server.js
    
    * Fixed login route
    
    * Removed default alert from registration form; removed JSON from login modal

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,9 +4,10 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const jobsRoutes = require("./jobsRoutes");
 const adzunaRoutes = require("./adzunaRoutes");
+const auth = require('./auth');
 
 router.use("/users", userRoutes);
-router.use("/jobs", jobsRoutes);
+router.use("/jobs", auth, jobsRoutes);
 router.use("/adzuna", adzunaRoutes);
 
 module.exports = router;
